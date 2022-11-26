7c0c04938a1882d3181f41203ce24bfea328cd4c
dkaszyns <01169181@pw.edu.pl>
2022-11-26 19:27:28 +0100

    Initialize chatup_server

---
+++ chatup_server/src/main/java/com/chatup/chatup_server/ChatupServerApplication.java
@@ -0,0 +1,13 @@
+package com.chatup.chatup_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatupServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatupServerApplication.class, args);
+    }
+
+}
