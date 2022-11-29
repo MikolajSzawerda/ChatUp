3ca8d9f6392c1400ba7e7eaa0f6ac3086aca23ea
dkaszyns <01169181@pw.edu.pl>
2022-11-29 03:03:32 +0100

    Add AppUser JPA repository

---
+++ chatup_server/src/main/java/com/chatup/chatup_server/repository/AppUserRepository.java
@@ -0,0 +1,11 @@
+package com.chatup.chatup_server.repository;
+
+import com.chatup.chatup_server.domain.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+    AppUser findAppUserByUsername(String username);
+}
