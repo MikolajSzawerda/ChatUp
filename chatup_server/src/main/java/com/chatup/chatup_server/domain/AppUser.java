67e6e343bdfbaff4689ab63200c86328e04a9eb9
dkaszyns <01169181@pw.edu.pl>
2022-11-29 03:01:21 +0100

    Define domain entities

---
+++ chatup_server/src/main/java/com/chatup/chatup_server/domain/AppUser.java
@@ -0,0 +1,103 @@
+package com.chatup.chatup_server.domain;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+
+@Entity
+@Table(
+        name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "username_unique", columnNames = {"username"})
+        },
+        indexes = {
+                @Index(name = "username_index", columnList = "username", unique=true)
+        }
+)
+public class AppUser implements UserDetails {
+    @Id
+    @SequenceGenerator(
+            name = "user_sequence",
+            sequenceName = "user_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_sequence"
+    )
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String status;
+
+    @ColumnDefault(value = "true")
+    private Boolean isEnabled;
+
+    public AppUser() {}
+
+    public AppUser(
+            Long id, String firstName, String lastName, String username,
+            String password, String status, Boolean isEnabled
+    ) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.status = status;
+        this.isEnabled = isEnabled;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    @Override
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    @Override
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    @Override
+    public boolean isEnabled() { return isEnabled; }
+    public void setIsEnabled(Boolean enabled) { isEnabled = enabled; }
+
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() { return null; }
+}
---
+++ chatup_server/src/main/java/com/chatup/chatup_server/domain/JwtRequest.java
@@ -0,0 +1,4 @@
+package com.chatup.chatup_server.domain;
+
+
+public record JwtRequest(String username, String password) {}
