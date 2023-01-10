3de4b62e1116e7d6b44b3e32101d4fdbc02e4b57
Mikołaj Szawerda <01169246@pw.edu.pl>
2023-01-10 08:01:13 +0100

    Search request is filtered only to user channels

--- chatup_server/src/main/java/com/chatup/chatup_server/api/SearchEndpoint.java
+++ chatup_server/src/main/java/com/chatup/chatup_server/api/SearchEndpoint.java
@@ -1,11 +1,13 @@
 package com.chatup.chatup_server.api;
 
+import com.chatup.chatup_server.domain.Channel;
 import com.chatup.chatup_server.domain.UserInfo;
 import com.chatup.chatup_server.service.AppUserService;
 import com.chatup.chatup_server.service.messaging.MessageService;
 import com.chatup.chatup_server.service.messaging.OutgoingMessage;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -23,8 +25,13 @@ public class SearchEndpoint {
     @GetMapping("/search/messages")
     List<OutgoingMessage> byContentSearch(@RequestParam("phrase") String phrase,
                                           @RequestParam(value = "page", defaultValue = "0") int page,
-                                          @RequestParam(value="channels") Set<Long> channels){
-        return messageService.searchByContent(phrase, channels, page)
+                                          @RequestParam(value="channels") Set<Long> channels,
+                                          Principal principal){
+        Set<Long> filteredChannels = appUserService.getUserChannelsByUsername(principal.getName()).stream()
+                .map(Channel::getId)
+                .filter(channels::contains)
+                .collect(Collectors.toSet());
+        return messageService.searchByContent(phrase, filteredChannels, page)
                 .stream()
                 .map(OutgoingMessage::from)
                 .collect(Collectors.toList());
